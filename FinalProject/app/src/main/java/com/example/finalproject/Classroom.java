e98cc108890b9cc867e7a84b3ee9f76db01d63f4
PlusUltra404 <71044053+PlusUltra404@users.noreply.github.com>
2021-04-14 09:03:45 -0400

    Classroom and outside views

--- FinalProject/app/src/main/java/com/example/finalproject/Classroom.java
+++ FinalProject/app/src/main/java/com/example/finalproject/Classroom.java
@@ -1,4 +1,14 @@
 package com.example.finalproject;
 
-public class Classroom {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Classroom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.classroom);
+    }
 }
