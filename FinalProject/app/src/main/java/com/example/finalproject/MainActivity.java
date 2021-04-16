9cbf98f4cb4053bdab9256e10093dd3de079ce03
joshlin5 <linjos6750@gmail.com>
2021-04-16 16:04:21 -0400

    Basic files created

--- FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
@@ -8,12 +12,23 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private UserDatabase mUserDb;
     private UserDao userDao;
+    Button buttonStart, buttonAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonStart = findViewById(R.id.startButton);
+        buttonAbout = findViewById(R.id.aboutButton);
+        buttonStart.setOnClickListener(v -> {
+            //Intent intent = new Intent(this, Hallway.class);
+            //startActivity(intent);
+        });
+        buttonAbout.setOnClickListener(v -> {
+            //Intent intent = new Intent(this, About.class);
+            //startActivity(intent);
+        });
         mUserDb = UserDatabase.getInstance(getApplicationContext());
         UserDao userDao = mUserDb.userDao();
     }
